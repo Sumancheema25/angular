2acb4fb9186379eac429a1db03065cd920288bf0
Sumancheema25 <47419429+Sumancheema25@users.noreply.github.com>
2019-03-11 12:46:43 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CreateTableComponent } from './create-table/create-table.component';
-
+import {ArrayService} from './array.service';
 @NgModule({
   declarations: [
     AppComponent,
     CreateTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ArrayService
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,CreateTableComponent]
 })
 export class AppModule { }
