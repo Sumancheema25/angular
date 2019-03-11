8dc32319904cc3b388e228688ab694f668f2a8e1
Sumancheema25 <47419429+Sumancheema25@users.noreply.github.com>
2019-03-11 12:42:56 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArrayService } from './array.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularTable';
+  constructor(private ArrayService: ArrayService) { }
+
+  mydata  = this.ArrayService.getarray();
+  heading = this.ArrayService.getcolumn();
+  ngOnInit() {
+  }
+
 }
